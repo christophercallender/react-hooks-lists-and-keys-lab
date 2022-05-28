634d0e98044423ea453bcd29a0f28f568a77410d
Christopher Callender <christophercallender0000@gmail.com>
2022-05-28 10:39:11 -0400

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React from 'react';
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+   const techMap = technologies.map((technology) => (
+      <span key={technology}>{technology}</span>
+   ));
+   return (
+      <div className="project-item">
+         <h3>{name}</h3>
+         <p>{about}</p>
+         <div className="technologies">{techMap}</div>
       </div>
-    </div>
-  );
+   );
 }
+
+//or
+// function ProjectItem({ name, about, technologies }) {
+//    return (
+//       <div className="project-item">
+//          <h3>{name}</h3>
+//          <p>{about}</p>
+//          <div className="technologies">
+//             {technologies.map((technology) => (
+//                <span key={technology}>{technology}</span>
+//             ))}
+//          </div>
+//       </div>
+//    );
+// }
 
 export default ProjectItem;
